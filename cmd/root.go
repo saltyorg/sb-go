f540a231f0714223743cc7d8686b125ca68ca5ce
saltydk <salty@saltbox.dev>
2025-12-10 07:07:41 +0100

    cmd: hide help command

--- cmd/root.go
+++ cmd/root.go
@@ -32,15 +32,7 @@ func ExecuteContext(ctx context.Context) error {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.github.com/saltyorg/sb-go.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.SetHelpCommand(&cobra.Command{Hidden: true}) // -h/--help flags are sufficient
 }
 
 // handleInterruptError checks if the error is from a user interrupt and triggers shutdown.
